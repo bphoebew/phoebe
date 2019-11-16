88e3e0d666c4d03c4b67e59749807ad7bec8965b
bphoebew <bphoebew@gmail.com>
2019-11-16 14:03:55 -0800

    commit 7

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
+$(document).ready(function($) {
+    var Body = $('body');
+    Body.addClass('preloader-site');
+});
+$(window).load(function() {
+    $('.preloader-wrapper').fadeOut();
+    $('body').removeClass('preloader-site');
+});
 
 //settings variables
 var white = $('.white');
